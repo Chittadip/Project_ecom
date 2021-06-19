2cd6e238a9b980e3e9e943e79fe86f35bbf6849a
shreyasi-prog <shreyasibatabyal353@gmail.com>
2021-06-19 15:30:47 +0530

    check

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
 <a href="register.php">Register</a>&nbsp;||&nbsp;
 <!--<a href="login.php">Login</a>&nbsp;||&nbsp;-->
 <a href="profile.php">Account Details</a>&nbsp;||&nbsp;
+<a href="homepage.php">Home page</a>&nbsp;||&nbsp;
 
 <h1>FOR SUPPLIERS</h1>
 <a href="sreg.php">Register Here</a>&nbsp;||&nbsp;
 <!--<a href="slogin.php">Login</a>&nbsp;||&nbsp;-->
 <a href="sprofile.php">Account Details</a>&nbsp;||&nbsp;
-<a href="pro_addfile.php">Add product</a>&nbsp;||&nbsp;
 <a href="pro_viewfile.php">View product</a>&nbsp;||&nbsp;
-<a href="pro_updtdfile.php">Update product</a>&nbsp;||&nbsp;
-<a href="pro_delfile.php">Delete product</a>&nbsp;||&nbsp;
 <?php
 
 if(isset($_REQUEST['msg'])) {
