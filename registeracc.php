a38b992da4546bec8d7832ee3b45539f4ff2f750
shreyasi-prog <shreyasibatabyal353@gmail.com>
2021-07-22 11:02:05 +0530

    chng password and check dup mail

--- registeracc.php
+++ registeracc.php
@@ -6,8 +6,14 @@ include("config.php");
 	$cus_name=$_REQUEST['name'];
 	$cus_email=$_REQUEST['email'];
 	$cus_password=$_REQUEST['password'];
-	$cus_phnno=$_REQUEST['phno'];
+	$cus_phnno=$_REQUEST['phnno'];
 	$path="nil";
+	$dup_email=$_REQUEST['email'];
+
+	$qry="SELECT ID FROM `customers` WHERE Email_ID='$dup_email'";
+	$q1=mysqli_query($dbcon,$qry);
+	$num=mysqli_num_rows($q1);
+	if($num===0){
 	
 	$sql="INSERT INTO `customers`(Name,Email_ID, Password,Phone_no,Image,Status) VALUES ('$cus_name','$cus_email','$cus_password','$cus_phnno','$path','active')";
 	echo $sql;
@@ -18,4 +24,7 @@ include("config.php");
 	}else{
 	echo "not inserted";
 	}
+}else{
+	echo "Email id already exists. please try a different email id.";
+}
 ?>
